7208ac8de952bc33a703bbb8b0f51a319388ed9d
Jester Lumacad <mr.jesterlumacad@gmail.com>
2024-08-22 13:07:08 +0800

    added code

--- scripts/data/dataset.py
+++ scripts/data/dataset.py
@@ -1,22 +1,32 @@
-import pandas as pd
 import os
+import pandas as pd
 
-# Get the current working directory
-pwd = os.getcwd()
+# Get the directory of the current script
+# Define the relative path to the CSV file
+# Construct the full path to the CSV file
+script_dir = os.path.dirname(os.path.abspath(__file__))
 
-# Get the parent directory (one level up)
-p_dir = os.path.dirname(pwd)
+listings_relative_path = os.path.join("..", "..", "data", "raw", "listings.csv")
+neighbourhoods_relative_path = os.path.join(
+    "..", "..", "data", "raw", "neighbourhoods.csv"
+)
 
-# Get the grandparent directory (two levels up)
-gp_dir = os.path.dirname(p_dir)
-print(gp_dir)
+listings_file_path = os.path.abspath(os.path.join(script_dir, listings_relative_path))
+neighbourhoods_file_path = os.path.abspath(
+    os.path.join(script_dir, neighbourhoods_relative_path)
+)
 
+# Read the CSV file into a DataFrame
+df_listings = pd.read_csv(listings_file_path)
+df_neighbourhoods = pd.read_csv(neighbourhoods_file_path)
 
-listings_path = os.path.join(gp_dir, "data", "raw", "listings.csv")
-neighborhoods_path = os.path.join(gp_dir, "data", "raw", "neighbourhoods.csv")
-
-listings = pd.read_csv(listings_path)
-neighborhoods = pd.read_csv(neighborhoods_path)
-
-listings.head()
-neighborhoods.head()
+# Print filepaths and DataFrames
+df_listings.head()
+df_neighbourhoods.head()
+print(df_listings.head())
+print(df_neighbourhoods.head())
+print(script_dir)
+print(listings_relative_path)
+print(neighbourhoods_relative_path)
+print(listings_file_path)
+print(neighbourhoods_file_path)

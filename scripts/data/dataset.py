5ddcb1f53458aad567cf712bea8b2644c44c345d
Jester Lumacad <mr.jesterlumacad@gmail.com>
2024-08-24 00:53:23 +0800

    additional changes to code

--- scripts/data/dataset.py
+++ scripts/data/dataset.py
@@ -1,6 +1,7 @@
 import os
-import pandas as pd
 import re
+
+import pandas as pd
 
 # Get the directory of the current script
 # Define the relative path to the CSV file
@@ -18,9 +19,11 @@ neighbourhoods_file_path = os.path.abspath(
 )
 
 # Read the CSV file into a DataFrame
-df_listings = pd.read_csv(listings_file_path)
-df_neighbourhoods = pd.read_csv(neighbourhoods_file_path)
+df1 = pd.read_csv(listings_file_path)
+df2 = pd.read_csv(neighbourhoods_file_path)
 
+df_listings = pd.DataFrame(df1)
+df_neighbourhoods = pd.DataFrame(df2)
 # Print filepaths and DataFrames for verification
 
 print(df_listings.head())
@@ -68,32 +71,35 @@ listings_clean.head()
 listings_clean.info()
 print(listings_clean.columns)
 
+# *TODO Create code to clean data dropping non-ASCII characters
+# # Define the function to check for English text
+# def is_english(text):
+#     # Regex pattern for English letters, numbers, and common English symbols
+#     pattern = r"^[A-Za-z0-9\s.,!?\'\"()@#$%^&*;:\[\]{}±_+=/\\|`~]*$"
+#     return bool(re.match(pattern, text))
 
-# Define the function to check for English text
-def is_english(text):
-    # Regex pattern for English letters, numbers, and common English symbols
-    pattern = r"^[A-Za-z0-9\s.,!?\'\"()@#$%^&*;:\[\]{}±_+=/\\|`~]*$"
-    return bool(re.match(pattern, text))
 
+# # Apply the function to check each relevant column and create a boolean mask
+# listings_clean["is_english"] = listings_clean.apply(
+#     lambda row: is_english(str(row.get("name", "")))
+#     if isinstance(row.get("name", ""), str)
+#     else False or is_english(str(row.get("host_name", "")))
+#     if isinstance(row.get("host_name", ""), str)
+#     else False,
+#     axis=1,
+# )
 
-# Apply the function to check each relevant column and create a boolean mask
-listings_clean["is_english"] = listings_clean.apply(
-    lambda row: is_english(str(row.get("name", "")))
-    if isinstance(row.get("name", ""), str)
-    else False or is_english(str(row.get("host_name", "")))
-    if isinstance(row.get("host_name", ""), str)
-    else False,
-    axis=1,
-)
+# # Delete rows where at least one relevant column contains non-English characters
+# df_listings_cleaned = listings_clean[listings_clean["is_english"]].drop(
+#     columns="is_english"
+# )
 
-# Delete rows where at least one relevant column contains non-English characters
-df_listings_cleaned = listings_clean[listings_clean["is_english"]].drop(
-    columns="is_english"
-)
+# print(df_listings_cleaned)
+# df_listings_cleaned
+# df_listings_cleaned.info()
 
-print(df_listings_cleaned)
-df_listings_cleaned
-df_listings_cleaned.info()
+# *TODO Create code to clean data without dropping non-ASCII characters
+# df_listings_cleaned = listings_clean??
 
 # Cleaning df_neighbourhoods
 neighbourhoods_clean = df_neighbourhoods.copy()
